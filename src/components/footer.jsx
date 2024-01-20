441a6b7a1fda963377b403937de6c3ae6e97383c
Edu980-8 <daniedu151@gmail.com>
2024-01-19 23:02:10 -0500

    Changing the footer, and deleting the App.js instead i place App.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import logo5ig from "./logo5ig.svg";
+import "../style/footer.css";
 
 const footer = () => {
   return (
-    <div>
-      
+    <div className="footer-container">
+      <div>
+        <img className="logo-5ig-footer" src={logo5ig} alt="Logo_5ig"></img>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default footer
+export default footer;
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,11 +2,13 @@
 // import './App.css';
 
 import Header from "../components/header";
+import Footer from "../components/footer"
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Header/>
+      <Footer/>
     </div>
   );
 }
