c0f682cc85fd7d1194a5f2444be485ef349e95b0
Edu980-8 <daniedu151@gmail.com>
2024-01-19 20:59:16 -0500

    Adding colors to the header and also margins and paddings to the navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import logo5ig from './logo5ig.svg'
-import home from './home.svg'
-import insta from './insta.svg'
-import linkedin from './linkedin.svg'
+import React from "react";
+import logo5ig from "./logo5ig.svg";
+import home from "./home.svg";
+import insta from "./insta.svg";
+import linkedin from "./linkedin.svg";
 
-import './header.css'
-
+import "./header.css";
 
 const header = () => {
   return (
     <div className="header-container">
-      <img src={home} alt="Home"></img>
-      <img src={logo5ig} alt="Home"></img>
-      <button>Trabaja con Nosotros </button>
-      <button>¿Quienes Somos?</button>
-      <button>Contáctanos</button>
-      <img src={insta} alt="Home"></img>
-      <img src={linkedin} alt="Home"></img>
+      <div className="header-home-container">
+        <img className="img-5ig" src={home} alt="Icono_5ig"></img>
+        <img className="logo-5ig" src={logo5ig} alt="Logo_5ig"></img>
+      </div>
+      <div className="header-contact-container">
+        <button className="header-button">Trabaja con Nosotros </button>
+        <button className="header-button">¿Quienes Somos?</button>
+        <button className="header-button">Contáctanos</button>
+        <div className="header-logos">
+          <img src={insta} alt="Home"></img>
+          <img src={linkedin} alt="Home"></img>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default header
+export default header;
