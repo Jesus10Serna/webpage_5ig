6d821002ff63e858d5b953a0ef760cf4365dedfe
Edu980-8 <daniedu151@gmail.com>
2024-01-20 07:26:28 -0500

    Adding styles to the footer and also a Layout

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import logo5ig from "./logo5ig.svg";
 import "../style/footer.css";
+import insta from "./insta.svg";
+import linkedin from "./linkedin.svg";
 
 const footer = () => {
   return (
     <div className="footer-container">
-      <div>
+      <div className="footer-info-container">
         <img className="logo-5ig-footer" src={logo5ig} alt="Logo_5ig"></img>
+        <div className="footer-general-info">
+          <p>¿Quienes somos?</p>
+          <p>Contáctanos</p>
+          <div className="footer-logos">
+            <img src={insta} alt="Home"></img>
+            <img src={linkedin} alt="Home"></img>
+          </div>
+        </div>
       </div>
     </div>
   );
