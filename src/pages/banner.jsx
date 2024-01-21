8c8a63a8f34633cdc94c9a6558d28c712f9fe908
Edu980-8 <daniedu151@gmail.com>
2024-01-21 13:13:33 -0500

    Adding space between buttons and also above titles in banner section

--- src/pages/banner.jsx
+++ src/pages/banner.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import "../style/banner.css"
-
+import React from "react";
+import "../style/banner.css";
 
 const banner = () => {
   return (
     <div className="banner-container">
       <div className="banner-title">
-        <h2>COMIENZA A</h2>
-        <h1>AUTOMATIZAR TU MUNDO</h1>
+        <div className="titles">
+          <h2>COMIENZA A</h2>
+          <h1>AUTOMATIZAR TU MUNDO</h1>
+        </div>
         <div className="banner-buttons">
-          <button>Trabaja Con Nosotros</button>
-          <button>Automatiza Tu Empresa</button>
+          <button className="yellow-button">Trabaja Con Nosotros</button>
+          <button className="blue-button">Automatiza Tu Empresa</button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default banner
+export default banner;
