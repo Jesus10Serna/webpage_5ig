3d2e316534735b6e8cf7d6797e6c660793a48733
Edward <edwardgta4@hotmail.com>
2024-01-21 15:51:17 -0500

    1

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,10 +7,13 @@ import linkedin from "./linkedin.svg";
 import "../style/header.css";
 
 const header = () => {
+  const handleRedirect = (route) => {
+    window.location.href = `/${route}`;
+  };
   return (
     <div className="header-container">
       <div className="header-home-container">
-        <img className="img-5ig" src={home} alt="Icono_5ig"></img>
+        <img className="img-5ig" src={home} onClick={()=>handleRedirect('')} alt="Icono_5ig"></img>
         <img className="logo-5ig" src={logo5ig} alt="Logo_5ig"></img>
       </div>
       <div className="header-contact-container">
