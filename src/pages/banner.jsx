bdb656665bb6586f8cc0151158380543a3217b54
Edward <edwardgta4@hotmail.com>
2024-01-21 15:28:36 -0500

    1

--- src/pages/banner.jsx
+++ src/pages/banner.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import "../style/banner.css";
 
 const banner = () => {
+  const handleRedirect = (route) => {
+    window.location.href = `/${route}`;
+  };
   return (
+    
+
+
     <div className="banner-container">
       <div className="banner-title">
         <div className="titles">
@@ -10,8 +16,8 @@ const banner = () => {
           <h1>AUTOMATIZAR TU MUNDO</h1>
         </div>
         <div className="banner-buttons">
-          <button className="yellow-button">Trabaja Con Nosotros</button>
-          <button className="blue-button">Automatiza Tu Empresa</button>
+          <button className="yellow-button" >Trabaja Con Nosotros</button>
+          <button className="blue-button" onClick={()=>handleRedirect('automate')}>Automatiza Tu Empresa</button>
         </div>
       </div>
     </div>
