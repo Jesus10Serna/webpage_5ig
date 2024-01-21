72a3e8df8fa4e3b4aff4512f17c67c26324b1a82
Edu980-8 <daniedu151@gmail.com>
2024-01-21 12:40:34 -0500

    Adding the banner and the titles, trying to set up buttons

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,13 +1,15 @@
 // import logo from './logo.svg';
 // import './App.css';
 
-import Header from "../Components/header";
-import Footer from "../Components/footer"
+import Header from "../components/header";
+import Footer from "../components/footer"
+import Banner from "../pages/banner"
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Banner/>
       <Footer/>
     </div>
   );
--- src/pages/banner.jsx
+++ src/pages/banner.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import "../style/banner.css"
+
 
 const banner = () => {
   return (
-    <div>
-      
+    <div className="banner-container">
+      <div className="banner-title">
+        <h2>COMIENZA A</h2>
+        <h1>AUTOMATIZAR TU MUNDO</h1>
+        <div className="banner-buttons">
+          <button>Trabaja Con Nosotros</button>
+          <button>Automatiza Tu Empresa</button>
+        </div>
+      </div>
     </div>
   )
 }
