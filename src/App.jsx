4f55565898a1d867f5ef5a0ab9106a6aeb167bdd
Edward <edwardgta4@hotmail.com>
2024-01-21 14:48:46 -0500

    1

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Layout from './Layout/Layout';
 import Home from './pages/App';
+import Automate from './pages/automate';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Layout>
         <Routes>
           <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/automate" element={<Automate/>}/>
         </Routes>
       </Layout>
     </BrowserRouter>
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <Banner/>
+      <Banner/>     
       <Footer/>
     </div>
   );
