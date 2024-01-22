6315e8196f3b0b4efca849167f044f84d8d964a8
Edu980-8 <daniedu151@gmail.com>
2024-01-22 18:41:42 -0500

    Adding images and give some styles to the services title.

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -5,12 +5,14 @@ import Header from "../components/header";
 import Footer from "../components/footer"
 import Banner from "../pages/banner"
 import Contact from "../components/contact_us"
+import Services from "../pages/services"
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Banner/>  
+      <Banner/>
+      <Services/> 
       <Contact/>   
       <Footer/>
     </div>
--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import "../style/services.css";
 
 const services = () => {
   return (
-    <div>
-               
+    <div className="services-container">
+      <div className="services-title">
+        <div className="line_1"></div>
+        <h2>SERVICIOS</h2>
+        <div className="line_2"></div>
+      </div>
+      <div className="services-portfolio">
+        <div className="services-portfolio-height ia"></div>
+        <div className="services-portfolio-height rpa"></div>
+        <div className="services-portfolio-height cloud"></div>
+        <div className="services-portfolio-height bi"></div>
+        <div className="services-portfolio-height wdad"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default services
+export default services;
