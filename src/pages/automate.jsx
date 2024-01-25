fca8dca02250f2743dce8557e27233703759b54c
Edward <edwardgta4@hotmail.com>
2024-01-24 21:52:15 -0500

    Automate page

--- src/pages/automate.jsx
+++ src/pages/automate.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import "../style/automate.css";
 import "../style/banner.css";
+import Brain from "../components/brain-automate.svg";
+import PowerPlatform from "../components/powerplatform-automate.svg";
+import Robot from "../components/robot-automate.svg";
+import Chip from "../components/chip-automate.svg";
 import WspIcon from "../components/wsp.svg";
 import Footer from "../components/footer";
 import Header from '../components/header';
@@ -20,11 +24,48 @@ function automate() {
     </div>
     <div className='content-automate'>
       <div className='automate-title'>
-        <h1>¿qué son los procesos automatizados?</h1>
+        <div className='mid-line-container'>      
+        </div>
+          <div>
+          <h1>¿qué son los procesos automatizados?</h1>
+          </div>
+        <div className='mid-line-container'>
+        </div>
       </div>
       <div className='automate-container'>
-        <div></div>
-        <div></div>
+        <div>
+          <div>
+            <h3>Los procesos automatizados son aquellos que se realizan mediante el uso de tecnologías como:</h3>
+          </div>
+          <div>
+            <div>
+              <img src={Brain} />
+              <h2>Inteligencia artificial</h2>
+            </div>
+            <div>
+            <img src={PowerPlatform} />
+              <h2>Microsoft Power Platform</h2>
+            </div>
+            <div>
+            <img src={Robot} />
+              <h2>Machine Learning</h2>
+            </div>
+            <div>
+            <img src={Chip} />
+              <h2>Plataformas Tecnológicas Innovadoras</h2>
+            </div>
+          </div>
+          <div>
+            <h3>Estos procesos permiten a las organizaciones mejorar su rendimiento, eficiencia y adaptabilidad, así como crear experiencias de cliente excepcionales y memorables.</h3>
+          </div>
+        </div>
+        <div>
+          <h2>Nuestros Clientes suelen querer</h2>
+          <div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
         <div></div>
         <div></div>
       </div>
