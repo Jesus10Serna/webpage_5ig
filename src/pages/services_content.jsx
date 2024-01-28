db292bb99277f509dc89a926fc1b58b9a1815fa7
Edu980-8 <daniedu151@gmail.com>
2024-01-28 17:39:23 -0500

    Adding dynamic container for services depending on the section that is calling services content.

--- src/pages/services_content.jsx
+++ src/pages/services_content.jsx
@@ -1,25 +1,44 @@
-import React from 'react'
+import React from "react";
 import Header from "../components/header";
-import Footer from "../components/footer"
-import {useEffect, useState} from 'react'
-import {useParams} from 'react-router-dom'
+import Footer from "../components/footer";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import '../style/services_content.css'
 
 const Services_content = () => {
-    const params = useParams(); 
-    const [service, setService] = useState('');
-    useEffect(()=>{
-        console.log("Params_Id:",params.service)
-        setService(params.service)
-      },[params.id])
+  const params = useParams();
+  const [service, setService] = useState("");
+  useEffect(() => {
+    console.log(params.service,".");
+    setService(params.service);
+  }, [params.service]);
 
+  let content;
+
+  if (params.service === "ia") {
+    content = (
+      <div className="content-services">
+        <div className="container_1"></div>
+        <div className="container_2"></div>
+        <div className="container_3"></div>
+      </div>
+    );
+  } else {
+    content = (
+      <div className="content-services">
+        <div className="container_1"></div>
+        <div className="container_2"></div>
+      </div>
+    );
+  }
 
   return (
     <div>
-        <Header></Header>
-        {<h1>Hola: {service}</h1>}
-        <Footer></Footer>
+      <Header></Header>
+      {content}
+      <Footer></Footer>
     </div>
-  )
-}
+  );
+};
 
-export default Services_content
+export default Services_content;
