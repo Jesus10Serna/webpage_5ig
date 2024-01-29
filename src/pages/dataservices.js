0412527dd29782c2a304fc9b8153de7f6b1a2926
Edu980-8 <daniedu151@gmail.com>
2024-01-28 22:59:05 -0500

    making changes in services content.

---
+++ src/pages/dataservices.js
@@ -0,0 +1,63 @@
+export const info = [
+  {
+    img_source: "",
+    title: "Plataforma de Análisis Predictivo",
+    info: "Ofrecemos una plataforma de análisis predictivo impulsada por IA y ML que ayuda a las empresas a tomar decisiones más informadas mediante la identificación de tendencias y patrones en sus datos.",
+  },
+  {
+    img_source: "",
+    title: "Asistente Virtual Inteligente",
+    info: "Desarrollamos asistentes virtuales personalizados que utilizan IA para mejorar la atención al cliente, automatizar respuestas y proporcionar soporte las 24 horas del día.",
+  },
+  {
+    img_source: "",
+    title: "Sistemas de Recomendación",
+    info: "Creamos sistemas de recomendación basados en ML que ayudan a las empresas a aumentar la retención de clientes y las ventas al ofrecer recomendaciones personalizadas.",
+  },
+  {
+    img_source: "",
+    title: "Automatización de Procesos Empresariales",
+    info: "Implementamos soluciones RPA para automatizar tareas repetitivas y manuales, lo que aumenta la eficiencia y reduce los errores humanos.",
+  },
+  {
+    img_source: "",
+    title: "Asesoría en Implementación de RPA",
+    info: "Brindamos consultoría y servicios de implementación de RPA para ayudar a las empresas a identificar oportunidades de automatización y diseñar flujos de trabajo automatizados.",
+  },
+  {
+    img_source: "",
+    title: "Migración a la Nube",
+    info: "Ayudamos a las empresas a migrar sus sistemas y datos a la nube, ofreciendo opciones de infraestructura escalable y segura.",
+  },
+  {
+    img_source: "",
+    title: "Gestión de la Nube",
+    info: "Proporcionamos servicios de gestión de la nube, monitoreo y optimización para garantizar un rendimiento óptimo y una seguridad sólida en entornos de nube.",
+  },
+
+  {
+    img_source: "",
+    title: "Dashboards Personalizados",
+    info: "Diseñamos dashboards y paneles de control personalizados que brindan información en tiempo real y análisis de datos para la toma de decisiones estratégicas.",
+  },
+
+  {
+    img_source: "",
+    title: "Análisis de Datos Avanzado",
+    info: "Utilizamos herramientas de BI para realizar análisis avanzados, como análisis de tendencias, segmentación de clientes y detección de anomalías.",
+  },
+
+  {
+    img_source: "",
+    title: "Desarrollo de Sitios Web Responsive",
+    info: "Creamos sitios web responsivos y atractivos que se adaptan a diferentes dispositivos y ofrecen una experiencia de usuario excepcional.",
+  },
+  {
+    img_source: "",
+    title: "Desarrollo de Aplicaciones Móviles",
+    info: "Diseñamos y desarrollamos aplicaciones móviles nativas y multiplataforma para satisfacer las necesidades específicas de nuestros clientes.",
+  },
+
+  
+  
+];
--- src/pages/services_content.jsx
+++ src/pages/services_content.jsx
@@ -3,31 +3,68 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import '../style/services_content.css'
+import "../style/services_content.css";
+import { info } from "../pages/dataservices.js";
 
 const Services_content = () => {
   const params = useParams();
   const [service, setService] = useState("");
   useEffect(() => {
-    console.log(params.service,".");
+    console.log(params.service, ".");
     setService(params.service);
   }, [params.service]);
 
   let content;
 
-  if (params.service === "ia") {
+  let startIdx, endIdx;
+
+  if (service === "ia") {
+    startIdx = 0;
+    endIdx = 3;
+  } else if (service === "rpa") {
+    startIdx = 3;
+    endIdx = 5;
+  } else if (service === "cloud") {
+    startIdx = 5;
+    endIdx = 7;
+  } else if (service === "bi") {
+    startIdx = 7;
+    endIdx = 9;
+  }
+ else if (service === "web-app-dev") {
+  startIdx = 9;
+  endIdx = 11;
+}
+
+
+
+  if (startIdx !== undefined && endIdx !== undefined) {
     content = (
       <div className="content-services">
-        <div className="container_1"></div>
-        <div className="container_3"></div>
-        <div className="container_2"></div>
+        <div className="parent_container">
+          {info.slice(startIdx, endIdx).map((item, index) => (
+            <div key={index} className={`container_${startIdx + index + 1}`}>
+              <img src={item.img_source} alt="Imagen" />
+              <h2>{item.title}</h2>
+              <p>{item.info}</p>
+            </div>
+          ))}
+        </div>
       </div>
     );
   } else {
     content = (
       <div className="content-services">
-        <div className="container_1"></div>
-        <div className="container_2"></div>
+        <div className="parent_container">
+          {/* Renderizar los primeros tres elementos por defecto */}
+          {info.slice(0, 3).map((item, index) => (
+            <div key={index} className={`container_${index + 1}`}>
+              <img src={item.img_source} alt="Imagen" />
+              <h2>{item.title}</h2>
+              <p>{item.info}</p>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
