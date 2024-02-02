3dc6b0feb1816dd8aaf008391ac94457e1b5354a
Edu980-8 <daniedu151@gmail.com>
2024-02-02 16:13:11 -0500

    Agregando la seccion de contrata con nosotros

--- src/pages/Contract_us.jsx
+++ src/pages/Contract_us.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
-import Header from '../components/header'
-import Footer from '../components/footer'
+import React from "react";
+import Header from "../components/header";
+import Footer from "../components/footer";
 import "../style/contract_us.css";
-
-
 
 const Contract_us = () => {
   return (
     <>
-        <Header></Header>
-        <div className='container-img'></div>
-        <div className='containerc-content'></div>
-        <Footer></Footer>
+      <Header></Header>
+      <div className="contact-us-container">
+        <div className="contact-us-total">
+          <div className="contact-us-set">
+            <p>Lleva tu negocio a la nueva era de la tecnología</p>
+            <button>Contrata Con Nosotros</button>
+          </div>
+          <div className="contact-us-forms">
+            <form>
+              <p>¿Quien nos escribe?</p>
+              <input className="input-width" placeholder="Nombre"></input>
+              <p>Regálanos tu correo</p>
+              <input className="input-width" placeholder="E-mail"></input>
+              <p>Regálanos tu Teléfono</p>
+              <input className="input-width" placeholder="E-mail"></input>
+              <p>Déjanos tu mensaje</p>
+              <input className="input-width" placeholder="Mensaje"></input>
+              <button type="submit">Enviar</button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Footer></Footer>
     </>
-    
-  )
-}
+  );
+};
 
-export default Contract_us
+export default Contract_us;
