a1bf6fdd6542d1425b2797c0cb6326432ea3c01b
Edu980-8 <daniedu151@gmail.com>
2024-02-05 16:37:19 -0500

    Adding sending message

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,27 +1,79 @@
-import "../components/form.css";
+import React, { useState } from 'react';
+import '../components/form.css';
 
 const Form = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone_prefix: '',
+    phone: '',
+    message: ''
+  });
+
+  const handleChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    
+    console.log('Formulario enviado:', formData);
+  };
+
   return (
     <div>
       <div className="contact-us-forms">
-        <form>
+        <form target="_blank" action="https://formsubmit.co/eduardortegon2398@gmail.com" method="POST">
           <p>¿Quién nos escribe?</p>
-          <input className="input-width" placeholder="Nombre"></input>
+          <input
+            className="input-width"
+            placeholder="Nombre"
+            name="name"
+            type='text'
+            // value={formData.name}
+            onChange={handleChange}
+            required
+          />
           <p>Regálanos tu correo</p>
-          <input className="input-width" placeholder="E-mail"></input>
+          <input
+            className="input-width"
+            placeholder="E-mail"
+            name="email"
+            type='email'
+            required
+            value={formData.email}
+            onChange={handleChange}
+          />
           <p>Regálanos tu Teléfono</p>
           <div className="phone_container">
-            <input className="input-width-domain" placeholder="+57"></input>
+            <input
+              className="input-width-domain"
+              placeholder="+57"
+              name="phone_prefix"
+              type='text'
+              required
+              value={formData.phone_prefix}
+              onChange={handleChange}
+            />
             <input
               className="input-width"
               id="phone"
               placeholder="Teléfono"
-            ></input>
+              name="phone"
+              type='text'
+              required
+              value={formData.phone}
+              onChange={handleChange}
+            />
           </div>
           <p>Déjanos tu mensaje</p>
           <textarea
             className="input-width message"
             placeholder="Mensaje"
+            name="message"
+            type='text'
+            value={formData.message}
+            onChange={handleChange}
           />
           <button type="submit">Enviar</button>
         </form>
